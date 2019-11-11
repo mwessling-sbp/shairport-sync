e4f49b7a6ba004ef4234c9925d8511bbb10e6aec
mwessling-sbp <mwessling@schubergphilis.com>
2019-11-11 17:24:11 +0100

    Fix2

--- mqtt.c
+++ mqtt.c
@@ -98,9 +98,9 @@ void mqtt_publish(char *topic, char *data, uint32_t length) {
   
   // PUblish in JSON format
   if (config.mqtt_publish_json) {
-      char *jvalue = NULL;
-      
+      cJSON *jvalue = NULL;
       cJSON *jmsg = NULL;
+
       if (jmsg == NULL) 
         { 
           debug(1, "[MQTT]: json object creation failed"); goto end; 
@@ -117,7 +117,7 @@ void mqtt_publish(char *topic, char *data, uint32_t length) {
       {
         debug(1, "[MQTT]: json msg string conversion failed");goto end;
       }
-      :end
+      end:
       cJSON_Delete(jmsg);
   } else {
     snprintf(fulltopic, strlen(config.mqtt_topic) + strlen(topic) + 2, "%s/%s", config.mqtt_topic,
